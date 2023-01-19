fe264a6d69024912536f1151d10495eb36cb6384
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-01-19 13:33:09 +0800

    m

--- genSrvCode.sh
+++ genSrvCode.sh
@@ -2,9 +2,17 @@
 
 set -e
 
+DIST=server
+
+# clean old files
+rm -rf $DIST/go
+rm -rf $DIST/api
+rm -rf $DIST/.swagger-codegen*
+
+
 docker run --rm -v `pwd`:/local swaggerapi/swagger-codegen-cli-v3 generate \
     -i /local/openapi.yaml \
     -l go-server \
-    -o /local/server
+    -o /local/${DIST}
 
 
