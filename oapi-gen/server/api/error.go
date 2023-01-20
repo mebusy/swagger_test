8aa787c4835660d506f3e2655dd16ce4b63101d0
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-01-20 11:30:59 +0800

    m

--- oapi-gen/server/api/error.go
+++ oapi-gen/server/api/error.go
@@ -6,8 +6,11 @@ import (
 )
 
 const (
-	ERR_NO_ERROR        = 0
-	ERR_VALIDATOR_ERROR = 10
+	ERR_NO_ERROR      = 0
+	ERR_UNIMPLEMENTED = 1
+
+	ERR_APIKEY_NOT_PROVIDED = 10
+	ERR_VALIDATOR_ERROR     = 20
 )
 
 // ResErrorCode is also a custom error type
--- oapi-gen/server/api/validator_func.go
+++ oapi-gen/server/api/validator_func.go
@@ -3,8 +3,8 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"errors"
-	// "fmt"
+	// "errors"
+	"fmt"
 
 	"net/http"
 	"strings"
@@ -17,32 +17,47 @@ func CustomAuthenticationFunc(c context.Context, input *openapi3filter.Authentic
 	// error returned here, will be wrapped and passed to the customer error handler
 
 	// Type:apiKey Description: Name:token In:header
-	if input.SecurityScheme == nil || input.SecurityScheme.Type != "apiKey" || input.SecurityScheme.In != "header" || input.SecurityScheme.Name != "token" {
-
-		return errors.New("apiKey not found, or not in header")
+	if input.SecurityScheme != nil && input.SecurityScheme.Type == "apiKey" && input.SecurityScheme.In == "header" && input.SecurityScheme.Name == "token" {
+		// we currently use apiKey in header
+		token := input.RequestValidationInput.Request.Header.Get(input.SecurityScheme.Name)
+		if token == "" {
+			// token not privided
+			return NewResError(ERR_APIKEY_NOT_PROVIDED, "apikey not privided")
+		}
+		// check token whether valid
+		// TODO
+		return nil
 	}
-	token := input.RequestValidationInput.Request.Header.Get(input.SecurityScheme.Name)
-	_ = token
-	// fmt.Printf("token: %s\n", token)
 
-	return nil
+	return NewResError(ERR_UNIMPLEMENTED, "unimplemented security scheme")
+}
+
+// validator will add some wrapper to the original error message
+var OAPI_ERROR_WRAPPER = []string{
+	"security requirements failed: ",
 }
 
 // validator error
 func CustomErrorHandler(w http.ResponseWriter, message string, statusCode int) {
+	// remote the oapi error wrapper
+	for _, v := range OAPI_ERROR_WRAPPER {
+		message = strings.ReplaceAll(message, v, "")
+	}
+
 	// validator error
 	err := Json2ResErrorCode(message)
+	fmt.Printf("%+v", err)
 	if err.Errcode > 0 {
 		// custom error
 		json.NewEncoder(w).Encode(err)
 		return
 	} else {
-		// shink validator error
+		// validator error, e.g. number outof range, is too long...
+		// shink it to leave only the key part
 		idx := strings.Index(message, "Error at")
 		if idx > 0 {
 			message = message[idx:]
 		}
-
 		var res = ResErrorCode{Errcode: ERR_VALIDATOR_ERROR, Errmsg: &message}
 		json.NewEncoder(w).Encode(res)
 		return
